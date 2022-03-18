ec72d6d4bbd371c07b336c319ef1de9cc05b72fd
alexispascual <ap.alexispascual@gmail.com>
2022-03-18 19:04:49 -0400

    Renaming file and reconfiguring buttons

--- scripts/zed_control.py
+++ scripts/zed_control.py
@@ -15,8 +15,10 @@ class ZedCamera(object):
         super(ZedCamera, self).__init__()
 
         # Initialize button mapping
-        self.capture_image_button = 5
-        self.initialize_camera_button = 9
+        self.capture_depth_map_button = 6
+        self.capture_depth_video_button = 7
+        self.stream_video_button = 8
+        self.toggle_camera_button = 9
 
         # Initialize zed object
         self.zed = None
@@ -35,24 +37,15 @@ class ZedCamera(object):
         # Initialize joy message subscribers
         rospy.Subscriber('/joy_teleop/joy', Joy, self.handle_joy_message, queue_size=3, buff_size=2**16)
         
-    def take_left_image(self):
-        # Capture and save image
-        image = sl.Mat()
-        runtime_parameters = sl.RuntimeParameters()
-        
-        if self.zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:   
-            # Get image and time stamp
-            self.zed.retrieve_image(image, sl.VIEW.LEFT)
-            timestamp = self.zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)
-            
-            # Get Image data and save to file
-            image_data = image.get_data()
-            file_name = f"zed_image_left_{timestamp}.jpg"
+    def handle_joy_message(self, joy_msg):
+        # Massive if statement to handle joy mesages
+        if joy_msg.buttons[self.capture_depth_map_button]:
+           self.capture_depth_map()
 
-            # Save image
-            cv2.imwrite(os.path.join(self.save_directory, file_name), image_data)
+        elif joy_msg.buttons[self.toggle_camera_button]:
+            self.toggle_camera()
             
-    def take_depth_map(self):
+    def capture_depth_map(self):
         # Initialize depth map and image objects
         depth = sl.Mat()
         image = sl.Mat()
@@ -82,24 +75,22 @@ class ZedCamera(object):
             # Save data
             self.save_data(self, image_data, depth_data, timestamp)
             
-    def handle_joy_message(self, joy_msg):
-        # Massive if statement to handle joy mesages
-        if joy_msg.buttons[self.capture_image_button]:
-           self.take_left_image()
-
-        elif joy_msg.buttons[self.initialize_camera_button]:
+    def stream_video(self):
+        # Capture and save image
+        image = sl.Mat()
+        runtime_parameters = sl.RuntimeParameters()
+        
+        if self.zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:   
+            # Get image and time stamp
+            self.zed.retrieve_image(image, sl.VIEW.LEFT)
+            timestamp = self.zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)
             
-            # Toggle camera
-            if not self.zed and not self.init_params:
-                
-                self.zed, self.init_params = self.initialize_camera()
-                rospy.loginfo("Camera initialized!")
-            else:
-                
-                self.zed.close()
-                self.zed = None
-                self.init_params = None
-                rospy.loginfo("Camera closed!")
+            # Get Image data and save to file
+            image_data = image.get_data()
+            file_name = f"zed_image_left_{timestamp}.jpg"
+
+            # Save image
+            cv2.imwrite(os.path.join(self.save_directory, file_name), image_data)
 
     def save_data(self, image_data, depth_data, timestamp):
         # Create folder for depth map/image pair
@@ -114,23 +105,39 @@ class ZedCamera(object):
         file_name = f"depth_map_{timestamp}.npy"
         np.save(file_name, depth_data)
         
-    def initialize_camera(self):
-        # Create a Camera object
-        zed = sl.Camera()
+    def toggle_camera(self):
+        
+        # Initialize zed object if it doesn't exist
+        # Otherwise, close the camera
+        if not self.zed and not self.init_params:
+            rospy.loginfo("Initializing camera!")
+            
+            # Create a Camera object
+            zed = sl.Camera()
 
-        # Create a InitParameters object and set configuration parameters
-        init_params = sl.InitParameters()
-        init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE  # Use PERFORMANCE depth mode
-        init_params.coordinate_units = sl.UNIT.METER  # Use meter units (for depth measurements)
-        init_params.camera_resolution = sl.RESOLUTION.HD1080  # Use HD1080 video mode
-        init_params.camera_fps = 30  # Set fps at 30
-
-        # Open the camera
-        if zed.open(init_params) != sl.ERROR_CODE.SUCCESS:
-            rospy.loginfo("Zed cam failed to initialize")
+            # Create a InitParameters object and set configuration parameters
+            init_params = sl.InitParameters()
+            init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE  # Use PERFORMANCE depth mode
+            init_params.coordinate_units = sl.UNIT.METER  # Use meter units (for depth measurements)
+            init_params.camera_resolution = sl.RESOLUTION.HD1080  # Use HD1080 video mode
+            init_params.camera_fps = 30  # Set fps at 30
+            
+            if zed.open(init_params) != sl.ERROR_CODE.SUCCESS:
+                rospy.loginfo("Zed cam failed to initialize")
+            else:
+                self.zed = zed
+                self.init_params = init_params
+            
+                rospy.loginfo("Camera initialized!")
+            
         else:
-            return zed
-
+            rospy.loginfo("Closing camera!")
+            
+            self.zed.close()
+            self.zed = None
+            self.init_params = None
+            
+            rospy.loginfo("Camera closed!")
 
     def start(self):
         # Keeps python from exiting until node is stopped
