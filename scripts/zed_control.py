385c9bbe5a9ced1e1dea38029c7c5a9261b2464d
alexispascual <ap.alexispascual@gmail.com>
2022-03-21 00:44:09 -0400

    Fixing timestamp call

--- scripts/zed_control.py
+++ scripts/zed_control.py
@@ -116,19 +116,19 @@ class ZedCamera(object):
 
     def save_data(self, image_data, depth_data, point_cloud_data, timestamp):
         # Create folder for depth map/image pair
-        directory = os.path.join(self.save_directory, f"{timestamp}")
+        directory = os.path.join(self.save_directory, f"{timestamp.getSeconds()}")
         os.makedirs(directory)
         
         # Save Image
-        file_name = f"zed_image_left_{timestamp}.jpg"
+        file_name = f"zed_image_left_{timestamp.getSeconds()}.jpg"
         cv2.imwrite(os.path.join(directory, file_name), image_data)
         
         # Save depth map
-        file_name = f"depth_map_{timestamp}.npy"
+        file_name = f"depth_map_{timestamp.getSeconds()}.npy"
         np.save(file_name, depth_data)
 
         # Save depth map
-        file_name = f"point_cloud_{timestamp}.npy"
+        file_name = f"point_cloud_{timestamp.getSeconds()}.npy"
         np.save(file_name, point_cloud_data)
         
         rospy.loginfo("Data saved!")
